180fe94a74143617d16d700bed5777552ad9383b
LeLoader <leloaderpro@gmail.com>
2025-02-07 11:42:23 +0100

    Red text when linked

--- Assets/Scripts/MOTS/PlayerWord.cs
+++ Assets/Scripts/MOTS/PlayerWord.cs
@@ -13,10 +13,13 @@ public class PlayerWord : WordBase
     [Header("General")]
     [SerializeField] float interactionRadius = 5;
     [SerializeField] Rigidbody2D rb;
+    [SerializeField] Collider2D coll;
+    [SerializeField] float distanceCheck;
     [SerializeField] float jumpForce = 3f;
     [SerializeField] float speedForce = 5f;
 
     const int WORDOBJECT_LAYERMASK = 7;
+    const int MAP_LAYERMASK = 8;
     ContactFilter2D contactFilter = new();
 
     private void Start()
@@ -35,6 +38,14 @@ public class PlayerWord : WordBase
         Jump();
     }
 
+    private bool IsTouchingGround()
+    {
+        Debug.DrawLine(transform.position, transform.position + Vector3.down * ((coll.bounds.size.y / 2) + distanceCheck), Color.red);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, (coll.bounds.size.y / 2) + distanceCheck, (int)Mathf.Pow(2, MAP_LAYERMASK) + (int)Mathf.Pow(2, WORDOBJECT_LAYERMASK));
+        return (hit.collider != null);
+    }
+
+
     private void Move()
     {
         if (Input.GetKey(KeyCode.A))
@@ -52,14 +63,15 @@ public class PlayerWord : WordBase
 
     private void Jump()
     {
-        if (!Input.GetKeyDown(KeyCode.Space)) return;
+        if (!Input.GetKeyDown(KeyCode.Space) || !IsTouchingGround()) return;
 
         rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        Unlink();
     }
 
     private void Use()
     {
-        if (!Input.GetKeyDown(KeyCode.E)) return;
+        if (!Input.GetKeyDown(KeyCode.E) || !IsTouchingGround()) return;
         List<Collider2D> results = new(); 
  
         if (Physics2D.OverlapCircle(transform.position, interactionRadius, contactFilter, results) > 0)
@@ -74,12 +86,20 @@ public class PlayerWord : WordBase
     {
         wordObject.Link(this);
         LinkedWordBase = wordObject;
+        foreach (WordModifier modifier in currentModifiers)
+        {
+            modifier.WordUI.Link();
+        }
     }
 
     private void Unlink()
     {
         if (LinkedWordBase != null)
         {
+            foreach (WordModifier modifier in currentModifiers)
+            {
+                modifier.WordUI.Unlink();
+            }
             ((WordObject)LinkedWordBase).Unlink();
             LinkedWordBase = null;
         }
