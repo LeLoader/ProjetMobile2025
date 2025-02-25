475357a573751829f5434aa6aa9593859fdb3e68
GuyomeC <146094089+GuyomeC@users.noreply.github.com>
2025-02-25 16:42:05 +0100

    [ADD] achivement + update VerifyCompleted

---
+++ Assets/Scripts/AchivementManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public static class AchivementManager
+{
+
+    public const string FirstTry            = " code de l'achivement : finir le niveau 1 ";
+    public const string PaperKnight         = " code de l'achivement : finir tous les niveaux";
+    public const string PaperKing           = " code de l'achivement : finir tous les niveaux en moins de 30 minutes ";
+    public const string PaperGod            = " code de l'achivement : finir tous les niveaux en moins de 15 minutes ";
+    public const string BetterLuckNextTime  = " code de l'achivement : utiliser le bouton replay ";
+    public const string perseverance        = " code de l'achivement : lancer le Jeu une deuxième fois ";
+    public const string Ragequit            = " code de l'achivement : réassayer 5 fois un niveau puis quitter le jeu ";
+    public const string BouleQuiRoule       = " code de l'achivement : faire rouler la boule sur une pente a grande vitesse";
+    public const string StickyStick         = " code de l'achivement : grimper 10 fois grâce aux blocs Sticky Tall";
+    public const string SpiderMan           = " code de l'achivement : rester collé a un mur pendant 15 secondes";
+    public const string Maniac              = " code de l'achivement : sauter 100 fois en 1 niveau";
+    public const string Really              = " code de l'achivement : Recommencer un niveau 100 fois.";
+    public const string KeyOfSuccess        = " code de l'achivement : Déverrouiller tout les niveaux";
+    public const string ForAnAchivement     = " code de l'achivement : Sauter 1 000 000 de fois en 1 niveau.";
+    public const string FloorIsLava         = " code de l'achivement : ne pas toucher le sol 1 min  (peut marcher sur des blocs).";
+    public const string Statue              = " code de l'achivement : ne pas bouger pendant 1 minute";
+    public const string LePoidsDesMots      = " code de l'achivement : avoir cumulé 50 mots dans la seringue";
+    public const string Ecrivain            = " code de l'achivement : avoir cumulé 100 mots dans la seringue ";
+    public const string Dramaturge          = " code de l'achivement : avoir cumulé 1000 mots dans la seringue ";
+
+}
