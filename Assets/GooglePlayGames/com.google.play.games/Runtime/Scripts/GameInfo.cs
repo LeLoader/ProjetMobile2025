c6f063def3771c307ce1d4a683a5c64c5b118425
LeLoader <61639484+LeLoader@users.noreply.github.com>
2025-02-27 22:13:59 +0100

    Googleplay stuff
    - Activation
    - Achievement "First Try" on level but doesn't work

--- Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
+++ Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
@@ -39,7 +39,7 @@ namespace GooglePlayGames {
         public const string ApplicationId = "1081428256488"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
         public const string WebClientId = ""; // Filled in automatically
-        public const string NearbyConnectionServiceId = "";
+        public const string NearbyConnectionServiceId = "com.google.play.games";
 
         public static bool ApplicationIdInitialized() {
             return !string.IsNullOrEmpty(ApplicationId) && !ApplicationId.Equals(ToEscapedToken(UnescapedApplicationId));
