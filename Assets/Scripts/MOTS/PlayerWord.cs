44eab5b10808f58e9ca604025e7c4062da695dff
GuyomeC <146094089+GuyomeC@users.noreply.github.com>
2025-02-28 12:03:49 +0100

    Merge branch 'dev' into Guillaume

--- Assets/Scripts/MOTS/PlayerWord.cs
+++ Assets/Scripts/MOTS/PlayerWord.cs
@@ -432,7 +432,7 @@ public class PlayerWord : WordBase
         }
         else
         {
-            if (OnSlope == true)
+            if (OnSlope == true && !IsJumping)
             {
                 rb.linearVelocityY = 0;
             }
