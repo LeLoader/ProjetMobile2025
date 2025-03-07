053d8a5014109538293729a8c677ef83615ebd8d
LeLoader <61639484+LeLoader@users.noreply.github.com>
2025-03-07 01:40:31 +0100

    Fix WordUI gridlayout to vertical, and removed ref

--- Assets/Scripts/MOTS/ObjectUI.cs
+++ Assets/Scripts/MOTS/ObjectUI.cs
@@ -6,6 +6,5 @@ public class ObjectUI : MonoBehaviour
 {
     [SerializeField] public PositionConstraint positionConstraint;
     [SerializeField] public RotationConstraint rotationConstraint;
-    [SerializeField] public GridLayoutGroup gridLayout;
     [SerializeField] public GameObject wrapper;
 }
