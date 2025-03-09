db4544363ed1ea7d9dbd4fd6634cf2d9c1bb86aa
LeLoader <61639484+LeLoader@users.noreply.github.com>
2025-03-09 02:03:11 +0100

    UnlockAllAchievements works better now

--- Assets/Chromorphos/Scripts/PlayGames/UnlockAllAchievemtns.cs
+++ Assets/Chromorphos/Scripts/PlayGames/UnlockAllAchievemtns.cs
@@ -7,9 +7,25 @@ public class UnlockAllLevels : MonoBehaviour
     [Button]
     public void UnlockAllLevelsMethod()
     {
+        foreach (Level level in SaveSystem._instance._levelData._level)
+        {
+            if (level._state == Level.LevelState.Blocked)
+            {
+                level._state = Level.LevelState.Unlock;
+            }
+        }
+
         foreach (GameObject gameObject in VerifyCompleted.Instance._levelBoutton)
         {
-            gameObject.GetComponentInChildren<Button>().interactable = true;
+            for (int i = gameObject.transform.childCount - 1; i >= 0; i--)
+            {
+                if (!gameObject.transform.GetChild(i).GetComponent<Text>() && !gameObject.transform.GetChild(i).GetComponent<Button>())
+                {
+                    Destroy(gameObject.transform.GetChild(i).gameObject);
+                }
+            }
         }
+
+        VerifyCompleted.Instance.Verify();
     }
 }
